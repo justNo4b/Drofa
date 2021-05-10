e301f38fc7a1b52f1519593e3f096b33fd11de95
justno4b <novosti-wxo@yandex.ru>
2021-05-10 11:26:39 +0300

    dont update at null

--- src/search.cc
+++ src/search.cc
@@ -312,7 +312,7 @@ inline void Search::_updateBeta(const Move move, Color color, int pMove, int ply
 	if (!(move.getFlags() & 0x63)) {
     _orderingInfo.updateKillers(ply, move);
     _orderingInfo.incrementHistory(color, move.getFrom(), move.getTo(), depth);
-    _orderingInfo.incrementCMhistory(pMove, move.getPieceType(), move.getTo(), depth);
+    if (pMove != 0) _orderingInfo.incrementCMhistory(pMove, move.getPieceType(), move.getTo(), depth);
     _orderingInfo.updateCounterMove(color, pMove, move.getMoveINT());
   }
 }
@@ -738,7 +738,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
           // In this case we lower our search history values
           // In order to improve ordering if some move was beaten at very high depth
           _orderingInfo.decrementHistory(board.getActivePlayer(), move.getFrom(), move.getTo(), depth);
-          _orderingInfo.decrementCMhistory(pMove, move.getPieceType(), move.getTo(), depth);
+          if (pMove != 0 )_orderingInfo.decrementCMhistory(pMove, move.getPieceType(), move.getTo(), depth);
         }
       } 
 
