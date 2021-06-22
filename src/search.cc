690b3cf5fe1356c627b313e505350b9433bc69be
justno4b <novosti-wxo@yandex.ru>
2021-06-22 13:19:50 +0300

    add multyCut to the search

--- src/search.cc
+++ src/search.cc
@@ -596,6 +596,8 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
               int score = _negaMax(sBoard, &thisPV, sDepth, sBeta - 1, sBeta, ply, false, pMove, true);
               if (sBeta > score){
                 tDepth++;
+              } else if (sBeta >= beta) {
+                return beta;
               }
             }
 
