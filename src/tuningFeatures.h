75b00fc8cd8316ce7d64ae0b09305f4e26645182
justNo4b <69092138+justNo4b@users.noreply.github.com>
2021-06-23 08:23:01 +0300

    Bonus when passer dont have pieces in front of it

--- src/tuningFeatures.h
+++ src/tuningFeatures.h
@@ -5,7 +5,7 @@
 #include "eval.h"
 #include "outposts.h"
 
-#define BIG_FEATURE_NUMBER (52)
+#define BIG_FEATURE_NUMBER (53)
 
 enum TuningType{
     ALL,
@@ -56,6 +56,7 @@ tFeature myFeatures [BIG_FEATURE_NUMBER] = {
     tFeature("KING_ATTACK_PAWN", false, &Eval::KING_ATTACK_PAWN, 1, 1, ALL),
     tFeature("PASSED_PAWN_RANKS", true, Eval::PASSED_PAWN_RANKS, 8, 4, ALL),
     tFeature("PASSED_PAWN_FILES", true, Eval::PASSED_PAWN_FILES, 8, 4, ALL),
+    tFeature("PASSED_PAWN_FREE", true, Eval::PASSED_PAWN_FREE, 7, 4, ALL),
     tFeature("KING_PASSER_DISTANCE_FRIENDLY", true, Eval::KING_PASSER_DISTANCE_FRIENDLY, 9, 4, ALL),
     tFeature("KING_PASSER_DISTANCE_ENEMY", true, Eval::KING_PASSER_DISTANCE_ENEMY, 9, 4, ALL),
     tFeature("ROOK_OPEN_FILE_BONUS", true, Eval::ROOK_OPEN_FILE_BONUS, 2, 10, ALL),
