6cc809752a040348c009a70bf746797beb8f2c80
justno4b <novosti-wxo@yandex.ru>
2021-09-01 00:07:48 +0300

    add comments and kick version up

--- src/eval.cc
+++ src/eval.cc
@@ -123,6 +123,8 @@ void Eval::init() {
       U64 kingAttack = Attacks::getNonSlidingAttacks(KING, square, WHITE);
       detail::KINGZONE[color][square] = color == WHITE ? sqv | kingAttack | (kingAttack << 8)
                                                        : sqv | kingAttack | (kingAttack >> 8);
+      // When king is on the side files (A and H) include
+      // squares on the C and F files to the kingzone
       if (_col(square) == 0){
         detail::KINGZONE[color][square] = detail::KINGZONE[color][square] | detail::KINGZONE[color][square] << 1;
       }else if (_col(square) == 7){
--- src/version.h
+++ src/version.h
@@ -3,6 +3,6 @@
 
 #define VER_MAJ 3
 #define VER_MIN 1
-#define VER_PATCH 26
+#define VER_PATCH 27
 
 #endif
