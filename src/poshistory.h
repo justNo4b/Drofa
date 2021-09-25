0875da53df7e922f578d0f1b14066aa404c7ac6f
justno4b <novosti-wxo@yandex.ru>
2021-09-25 12:19:30 +0300

    class

--- src/poshistory.h
+++ src/poshistory.h
@@ -7,11 +7,17 @@
 class Poshistory
 {
     private:
-
-        U64         hisKey[MAX_GAME_PLY];
-        uint16_t    head;
+        U64         _hisKey[MAX_GAME_PLY];
+        int         _moves[MAX_GAME_PLY];
+        int         _statEval[MAX_GAME_PLY];
+        uint16_t    _ply;
+        uint16_t    _head;
 
     public:
+
+        void AddNode(U64, int);
+        void RemoveLast();
+        void UpdateEval();
 
 };
 
