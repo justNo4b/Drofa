c67c310da79b88e61fda29bce4e2f3134a8e0342
justno4b <novosti-wxo@yandex.ru>
2021-09-25 03:12:47 +0300

    define class

---
+++ src/poshistory.h
@@ -0,0 +1,19 @@
+#ifndef POSHISTORY_H
+#define POSHISTORY_H
+
+#include "defs.h"
+
+
+class Poshistory
+{
+    private:
+
+        U64         hisKey[MAX_GAME_PLY];
+        uint16_t    head;
+
+    public:
+
+};
+
+
+#endif
