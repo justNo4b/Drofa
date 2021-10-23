3b18d518c401422e0333395b060d1032b440887d
justNo4b <69092138+justNo4b@users.noreply.github.com>
2021-10-23 20:59:12 +0300

    fix eval term

--- src/eval.h
+++ src/eval.h
@@ -106,6 +106,8 @@ extern U64 KING_PAWN_MASKS[2][2][8];
 
 const int TEMPO = 10;
 
+const int KING_HIGH_DANGER = gS(-52,-63);
+
 const int KING_MED_DANGER = gS(-22,-36);
 
 const int KING_LOW_DANGER = gS(65,-64);
