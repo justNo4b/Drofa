2837dcd4c9d70e48da690b4f3082c60e3143d767
justno4b <novosti-wxo@yandex.ru>
2021-10-27 14:29:33 +0300

    allow more extentions when already extended

--- src/search.cc
+++ src/search.cc
@@ -671,8 +671,8 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
           // We finished reduction tweaking, calculate final depth and search
           // Idea from SF - LMR extensions
           // Allow reductions to be negative, allowing extending moves
-          int minReduction = 0;
-          minReduction += (!isQuiet) ? -2 : -1;
+          int minReduction = -1;
+          minReduction += (tDepth > depth) ? -2 : -1;
           reduction = std::max(minReduction, reduction);
           //Avoid to reduce so much that we go to QSearch right away
           int fDepth = std::max(1, tDepth - 1 - reduction);
