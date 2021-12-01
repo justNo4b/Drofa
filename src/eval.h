e4fceffc02cd66224d1c98bbc3e0f7443fec061c
justno4b <novosti-wxo@yandex.ru>
2021-12-01 14:05:22 +0300

    checks value tweak

--- src/eval.h
+++ src/eval.h
@@ -102,7 +102,7 @@ extern U64 KING_PAWN_MASKS[2][2][8];
   };
 
   const int PIECE_CHECK_POWER[6] = {
-        0, 22, 16, 92, 34, 0
+        0, 22, 92, 16, 34, 0
   };
 
   const int COUNT_TO_POWER[8] = {0, 0, 51, 83, 102, 111, 122, 128};
