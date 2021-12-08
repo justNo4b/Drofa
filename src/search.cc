2d7e367af929cccf39d2855e3153323cd8b54d5b
justno4b <novosti-wxo@yandex.ru>
2021-12-08 17:29:42 +0300

    less LMR extentions when no tt move on high depth

--- src/search.cc
+++ src/search.cc
@@ -676,6 +676,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
           // Idea from SF - > allow extending if our reductions are very negative
           int minReduction = 0;
           minReduction += (!isQuiet && LegalMoveCount <= 6) ? -2 : -1;
+          minReduction += (!TTmove && depth >= 7);
 
           reduction = std::max(minReduction, reduction);
           //Avoid to reduce so much that we go to QSearch right away
