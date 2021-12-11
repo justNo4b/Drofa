6a579940d35b8605a621ef7fc9de96fd98a8df37
justno4b <novosti-wxo@yandex.ru>
2021-12-11 22:04:00 +0300

    undo untestd

--- src/search.cc
+++ src/search.cc
@@ -633,7 +633,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
 
         // 8. LATE MOVE REDUCTIONS
         // mix of ideas from Weiss code, own ones and what is written in the chessprogramming wiki
-        doLMR = tDepth > 2 && LegalMoveCount > 2 + pvNode - TTmove;
+        doLMR = tDepth > 2 && LegalMoveCount > 2 + pvNode;
         if (doLMR){
 
           //Basic reduction is done according to the array
