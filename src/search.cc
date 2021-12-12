43b15914c9f498d6a10a9df10808e43444488112
justno4b <novosti-wxo@yandex.ru>
2021-12-12 21:44:41 +0300

    no kh for captures

--- src/search.cc
+++ src/search.cc
@@ -750,7 +750,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
             _orderingInfo.decrementHistory(board.getActivePlayer(), move.getFrom(), move.getTo(), dBonus);
             _orderingInfo.decrementCounterHistory(board.getActivePlayer(), pMove, move.getPieceType(), move.getTo(), dBonus);
           }else{
-            _orderingInfo.decrementCapHistory(move.getPieceType(), move.getCapturedPieceType(), move.getTo(), dBonus);
+            _orderingInfo.decrementCapHistory(move.getPieceType(), move.getCapturedPieceType(), move.getTo(), depth);
           }
         }
       }
