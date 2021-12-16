41c424091a175344fd98a461cb4a98313fbcd55b
justno4b <novosti-wxo@yandex.ru>
2021-12-17 02:39:07 +0300

    d1 fix

--- src/search.cc
+++ src/search.cc
@@ -570,7 +570,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
           && isQuiet
           && board.Calculate_SEE(move) < -51 * depth) continue;
 
-      if (depth == 10
+      if (depth == 1
           && !isQuiet
           && board.Calculate_SEE(move) < 0) continue;
     }
