161e40f1915786ff5af7659010557924ed267bfb
justno4b <novosti-wxo@yandex.ru>
2021-12-22 14:36:56 +0300

    version kick up

--- src/eval.cc
+++ src/eval.cc
@@ -316,7 +316,7 @@ inline int Eval::evaluateQUEEN(const Board & board, Color color, evalBits * eB){
       eB->KingAttackers[color]++;
       eB->KingAttackPower[color] += kingAttack * PIECE_ATTACK_POWER[QUEEN];
       eB->KingAttackPower[color] += (kingChecksCount - KingFaceChecksCount) * PIECE_CHECK_POWER[QUEEN];
-      eB->KingAttackPower[color] += KingFaceChecksCount * 80;
+      eB->KingAttackPower[color] += KingFaceChecksCount * QUEEN_FACE_CHECK;
     }
 
     // Save our attacks for further use
