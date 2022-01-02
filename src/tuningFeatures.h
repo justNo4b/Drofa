516b15b1760a04ae261b8eb91e58c739da7c77bd
justNo4b <69092138+justNo4b@users.noreply.github.com>
2022-01-02 21:17:13 +0300

    Add files via upload

--- src/tuningFeatures.h
+++ src/tuningFeatures.h
@@ -5,7 +5,7 @@
 #include "eval.h"
 #include "outposts.h"
 
-#define BIG_FEATURE_NUMBER (67)
+#define BIG_FEATURE_NUMBER (68)
 
 enum TuningType{
     ALL,
@@ -36,6 +36,7 @@ tFeature myFeatures [BIG_FEATURE_NUMBER] = {
     tFeature("ISOLATED_PAWN_PENALTY", false, &Eval::ISOLATED_PAWN_PENALTY, 1, 1, ALL),
     tFeature("PAWN_BLOCKED", false, &Eval::PAWN_BLOCKED, 1, 1, ALL),
     tFeature("PASSER_BLOCKED", false, &Eval::PASSER_BLOCKED, 1, 1, ALL),
+    tFeature("PAWN_PUSH_THREAT", false, &Eval::PAWN_PUSH_THREAT, 1, 1, ALL),
     tFeature("BISHOP_RAMMED_PENALTY", false, &Eval::BISHOP_RAMMED_PENALTY, 1, 1, ALL),
     tFeature("BISHOP_CENTER_CONTROL", false, &Eval::BISHOP_CENTER_CONTROL, 1, 1, ALL),
     tFeature("BISHOP_POS_PROUTPOST_JUMP", false, &Eval::BISHOP_POS_PROUTPOST_JUMP, 1, 1, ALL),
