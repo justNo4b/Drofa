edb379efb2eeb17c49a502f736fa789ffd814bc8
justno4b <novosti-wxo@yandex.ru>
2022-01-04 20:32:57 +0300

    just dont always be first

--- src/movepicker.cc
+++ src/movepicker.cc
@@ -33,7 +33,7 @@ void MovePicker::_scoreMoves(const Board *board) {
       }
       move.setValue(value);
     } else if (move.getFlags() & Move::PROMOTION) {
-      int value = _ply == MAX_PLY ? board->Calculate_SEE(move) :  PROMOTION_SORT[move.getPromotionPieceType()];
+      int value = _ply == MAX_PLY ? PROMOTION_SORT[move.getPromotionPieceType()] / 128 :  PROMOTION_SORT[move.getPromotionPieceType()];
       move.setValue(value);
     } else if (moveINT == Killer1) {
       move.setValue(KILLER1_BONUS);
