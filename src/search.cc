cebe24620168125c8543f8ef3b1c61af10b06b34
justno4b <novosti-wxo@yandex.ru>
2022-01-17 03:33:46 +0300

    only bonus in history for quiets

--- src/search.cc
+++ src/search.cc
@@ -767,7 +767,8 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
         // Beta cutoff
         if (score >= beta) {
           // Add this move as a new killer move and update history if move is quiet
-          _updateBeta(isQuiet, move, board.getActivePlayer(), pMove, ply, (depth + 2 * (statEVAL < alpha)));
+          int hBonus = depth + 2 * (statEVAL < alpha) * isQuiet;
+          _updateBeta(isQuiet, move, board.getActivePlayer(), pMove, ply, hBonus);
           // Add a new tt entry for this node
           if (!_stop && !sing){
             myHASH->HASH_Store(board.getZKey().getValue(), move.getMoveINT(), BETA, score, depth, ply);
