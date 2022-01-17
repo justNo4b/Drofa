a46d2dfcd9b4d40ed090c6c95a84beb3020e9cd6
justno4b <novosti-wxo@yandex.ru>
2022-01-17 04:04:02 +0300

    no pc in check, but it is on it singular search

--- src/search.cc
+++ src/search.cc
@@ -533,7 +533,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
   MovePicker movePicker(&_orderingInfo, &board, &legalMoves, hashedMove.getMoveINT(), board.getActivePlayer(), ply, pMove);
 
   // Probcut
-  if (!pvNode && depth >= 5 && alpha < WON_IN_X){
+  if (!pvNode && !AreWeInCheck && depth >= 5 && alpha < WON_IN_X){
     int pcBeta = beta + 200;
     while (movePicker.hasNext()){
       Move move = movePicker.getNext();
