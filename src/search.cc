a2cb7084a85721df342a397a2ad11cec6d8717ea
justno4b <novosti-wxo@yandex.ru>
2022-04-01 17:14:56 +0300

    35 value

--- src/search.cc
+++ src/search.cc
@@ -537,7 +537,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
 
       if (depth <= 6
           && !isQuiet
-          && getSee(move.getValue()) <= -25 * depth * depth) continue;
+          && getSee(move.getValue()) <= -35 * depth * depth) continue;
     }
 
     Board movedBoard = board;
