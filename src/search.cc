67f07af7c7fcaaf2bef1dfe6ba0128fd8f5660f4
justno4b <novosti-wxo@yandex.ru>
2022-04-07 02:06:21 +0300

    cap updates

--- src/search.cc
+++ src/search.cc
@@ -215,6 +215,7 @@ bool Search::_checkLimits() {
 }
 
 inline void Search::_updateBeta(bool isQuiet, const Move move, Color color, int pMove, int ply, int depth){
+    depth = std::min(15, depth);
 	if (isQuiet) {
     _orderingInfo.updateKillers(ply, move);
     _orderingInfo.incrementHistory(color, move.getFrom(), move.getTo(), depth);
@@ -720,6 +721,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
         }else{
           // Beta was not beaten and we dont improve alpha in this case we lower our search history values
           int dBonus = std::max(0, depth - (statEVAL < alpha) - (!TTmove && depth >= 4));
+          dBonus = std::min(15, dBonus);
           if (isQuiet){
             _orderingInfo.decrementHistory(board.getActivePlayer(), move.getFrom(), move.getTo(), dBonus);
             _orderingInfo.decrementCounterHistory(board.getActivePlayer(), pMoveIndx, move.getPieceType(), move.getTo(), dBonus);
