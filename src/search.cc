2012f40285ee915b3acc3eb6dd573a4fbcd95a07
justno4b <novosti-wxo@yandex.ru>
2022-04-07 16:54:30 +0300

    less

--- src/search.cc
+++ src/search.cc
@@ -719,7 +719,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
 
         }else{
           // Beta was not beaten and we dont improve alpha in this case we lower our search history values
-          int dBonus = std::max(0, depth - (statEVAL < alpha) - (!TTmove && depth >= 4));
+          int dBonus = std::max(0, depth - (statEVAL < alpha) - (!TTmove && depth >= 4) - singularExists);
           if (isQuiet){
             _orderingInfo.decrementHistory(board.getActivePlayer(), move.getFrom(), move.getTo(), dBonus);
             _orderingInfo.decrementCounterHistory(board.getActivePlayer(), pMoveIndx, move.getPieceType(), move.getTo(), dBonus);
