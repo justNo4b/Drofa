f7eccbd0e830a7f6cf6eb7fc92db2954454def3f
justno4b <novosti-wxo@yandex.ru>
2022-04-08 00:47:20 +0300

    tweak

--- src/search.cc
+++ src/search.cc
@@ -627,7 +627,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
           // Reduce more in the cut-nodes - used by SF/Komodo/etc
           reduction += cutNode;
 
-          reduction -= pMoveScore < -8192;
+          reduction -= pMoveScore < -16384;
 
           // if we are improving, reduce a bit less (from Weiss)
           reduction -= improving;
