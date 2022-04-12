8c630dd1e9566d36fdc78ea87887c567fc763cce
justno4b <novosti-wxo@yandex.ru>
2022-04-12 13:24:36 +0300

    pre-calculate history bonuses

--- src/search.cc
+++ src/search.cc
@@ -500,6 +500,8 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
   int  qCount = 0;
   bool singularExists = false;
   int  pMoveIndx = (pMove & 0x7) + ((pMove >> 15) & 0x3f) * 6;
+  int  mHistBonus = depth + 2 * (statEVAL < alpha);
+  int  mHistPenalty = std::max(0, depth - (statEVAL < alpha) - (!TTmove && depth >= 4));
 
   while (movePicker.hasNext()) {
     Move move = movePicker.getNext();
@@ -694,7 +696,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
         // Beta cutoff
         if (score >= beta) {
           // Add this move as a new killer move and update history if move is quiet
-          _updateBeta(isQuiet, move, board.getActivePlayer(), pMove, ply, (depth + 2 * (statEVAL < alpha)));
+          _updateBeta(isQuiet, move, board.getActivePlayer(), pMove, ply, mHistBonus);
           // Add a new tt entry for this node
           if (!_stop && !sing){
             myHASH->HASH_Store(board.getZKey().getValue(), move.getMoveINT(), BETA, score, depth, ply);
@@ -724,12 +726,11 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
 
         }else{
           // Beta was not beaten and we dont improve alpha in this case we lower our search history values
-          int dBonus = std::max(0, depth - (statEVAL < alpha) - (!TTmove && depth >= 4));
           if (isQuiet){
-            _orderingInfo.decrementHistory(board.getActivePlayer(), move.getFrom(), move.getTo(), dBonus);
-            _orderingInfo.decrementCounterHistory(board.getActivePlayer(), pMoveIndx, move.getPieceType(), move.getTo(), dBonus);
+            _orderingInfo.decrementHistory(board.getActivePlayer(), move.getFrom(), move.getTo(), mHistPenalty);
+            _orderingInfo.decrementCounterHistory(board.getActivePlayer(), pMoveIndx, move.getPieceType(), move.getTo(), mHistPenalty);
           }else{
-            _orderingInfo.decrementCapHistory(move.getPieceType(), move.getCapturedPieceType(), move.getTo(), dBonus);
+            _orderingInfo.decrementCapHistory(move.getPieceType(), move.getCapturedPieceType(), move.getTo(), mHistPenalty);
           }
         }
       }
