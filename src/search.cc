f56acac9f6054b59836b8e6ed33650b562caa126
justno4b <novosti-wxo@yandex.ru>
2022-07-10 02:06:34 +0300

    tweak for caps

--- src/search.cc
+++ src/search.cc
@@ -320,6 +320,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
   int pMoveScore = _sStack.moves[ply - 1].getValue();
   int alphaOrig = alpha;
   int statEVAL = 0;
+  bool pMoveIsQuiet = _sStack.moves[ply - 1].isQuiet();
   Move hashedMove = Move(0);
   pV   thisPV = pV();
   Color behindColor = _sStack.sideBehind;
@@ -617,7 +618,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
 
           // Reduce less if move on the previous ply was bad
           // Ie hystorycally bad quiet, see- capture or underpromotion
-          reduction -= pMoveScore < -8192;
+          reduction -= (pMoveIsQuiet && pMoveScore < -8192) || (!pMoveIsQuiet && pMoveScore < -108192) ;
 
           // if we are improving, reduce a bit less (from Weiss)
           reduction -= improving;
