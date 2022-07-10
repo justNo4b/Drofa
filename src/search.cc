6c2e0cc6773b0ae00578c49257fae578fb07faa1
justno4b <novosti-wxo@yandex.ru>
2022-07-10 03:27:03 +0300

    additional bonus for refuting countermove

--- src/search.cc
+++ src/search.cc
@@ -683,6 +683,9 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
         if (score >= beta) {
           // Add this move as a new killer move and update history if move is quiet
           _updateBeta(isQuiet, move, board.getActivePlayer(), pMove, ply, (depth + 2 * (statEVAL < alpha)));
+
+          if (pMoveScore > 50000 && pMoveScore < 200000)
+            _orderingInfo.incrementCounterHistory(board.getActivePlayer(), pMove, move.getPieceType(), move.getTo(), depth / 2);
           // Add a new tt entry for this node
           if (!_stop && !sing){
             myHASH->HASH_Store(board.getZKey().getValue(), move.getMoveINT(), BETA, score, depth, ply);
