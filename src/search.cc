6b00c6dbbb6cee2a1f3de179251feb5bc36cb3ed
justno4b <novosti-wxo@yandex.ru>
2022-07-15 15:50:27 +0300

    tweak

--- src/search.cc
+++ src/search.cc
@@ -682,9 +682,10 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
         // Beta cutoff
         if (score >= beta) {
           // Add this move as a new killer move and update history if move is quiet
-          _updateBeta(isQuiet, move, board.getActivePlayer(), pMove, ply, (depth + 2 * (statEVAL < alpha)));
+          int bonus = (depth + 2 * (statEVAL < alpha));
+          _updateBeta(isQuiet, move, board.getActivePlayer(), pMove, ply, bonus);
           if (pMoveScore >= 50000 && pMoveScore <= 200000)
-            _orderingInfo.incrementCounterHistory(board.getActivePlayer(), pMove, move.getPieceType(), move.getTo(), depth);
+            _orderingInfo.incrementCounterHistory(board.getActivePlayer(), pMove, move.getPieceType(), move.getTo(), bonus);
           // Add a new tt entry for this node
           if (!_stop && !sing){
             myHASH->HASH_Store(board.getZKey().getValue(), move.getMoveINT(), BETA, score, depth, ply);
