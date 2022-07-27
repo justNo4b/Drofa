94e1426eaac5e7b3fde79eb6e653341cc0848bb7
justno4b <novosti-wxo@yandex.ru>
2022-07-27 11:09:00 +0300

    do a re-search only in endgame position

--- src/search.cc
+++ src/search.cc
@@ -428,7 +428,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
           _sStack.RemoveNull(behindColor, nmpTree);
           if (score >= beta){
 
-            if (!board.isEndGamePosition() || depth <= 10) return beta;
+            if (!board.isEndGamePosition() && depth <= 10) return beta;
             // do a reSearch with nmp disabled for both colors at high
             // depth to verify not being in zugzwang
             _sStack.nmpDisabled = true;
