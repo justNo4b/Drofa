e6c85bcb0680306c06dabadf6930b3ff9fb3bb83
justno4b <novosti-wxo@yandex.ru>
2022-07-28 19:43:15 +0300

    measure Razoring

--- src/search.cc
+++ src/search.cc
@@ -395,13 +395,6 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
   // We do not do them InCheck, in pvNodes and when proving singularity
   bool isPrune = !pvNode && !AreWeInCheck && !sing;
 
-  // 1. RAZORING
-  // In the very leaf nodes (d == 1) with stat eval << beta we can assume that no
-  // Quiet move can beat it and drop to the QSearch immidiately
-  if (isPrune && depth == 1 && (statEVAL + RAZORING_MARGIN < beta)){
-        return _qSearch(board, alpha, beta);
-      }
-
   // 2. REVERSE FUTILITY
   // The idea is so if we are very far ahead of beta at low
   // depth, we can just return estimated eval (eval - margin),
