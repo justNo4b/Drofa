2137221d1e6d1657704b50e5cbcbfc4c21f54d6b
justno4b <novosti-wxo@yandex.ru>
2022-07-29 18:47:28 +0300

    more penalty

--- src/search.cc
+++ src/search.cc
@@ -714,7 +714,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
 
         }else{
           // Beta was not beaten and we dont improve alpha in this case we lower our search history values
-          int dBonus = std::max(0, depth - (statEVAL < alpha) - (!TTmove && depth >= 4) + failedNull);
+          int dBonus = std::max(0, depth - (statEVAL < alpha) - (!TTmove && depth >= 4) + 2 * failedNull);
           if (isQuiet){
             _orderingInfo.decrementHistory(board.getActivePlayer(), move.getFrom(), move.getTo(), dBonus);
             _orderingInfo.decrementCounterHistory(board.getActivePlayer(), pMoveIndx, move.getPieceType(), move.getTo(), dBonus);
