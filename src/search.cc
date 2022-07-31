367d1878773b5831317533b7eba39201d5321c97
justno4b <novosti-wxo@yandex.ru>
2022-07-31 16:15:47 +0300

    kick version up

--- src/search.cc
+++ src/search.cc
@@ -523,6 +523,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
           && isQuiet
           && board.Calculate_SEE(move) < -51 * depth) continue;
 
+      // 6. Prune quiet moves with poor CMH on the tips of the tree
       if (depth == 1 && isQuiet && cmHistory <= -4096) continue;
     }
 
