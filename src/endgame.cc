86820b29e9a098a4386014923658ded06353583d
justno4b <novosti-wxo@yandex.ru>
2022-08-23 19:19:39 +0300

    add basic evals

---
+++ src/endgame.cc
@@ -0,0 +1,72 @@
+#include "defs.h"
+#include "eval.h"
+#include "attacks.h"
+#include "movegen.h"
+#include "endgame.h"
+
+
+egEvalEntry myEvalHash [EG_HASH_SIZE];
+
+
+int Eval::evaluateDraw(const Board &board, Color color){
+    return 0;
+}
+
+inline void Eval::egHashAdd(std::string psFen, egEvalFunction ef){
+    ZKey key;
+    key.setpKeyFromString(psFen);
+    U64 index = key.getValue() & (EG_HASH_SIZE - 1);
+    if (myEvalHash[index].eFunction != nullptr){
+        std::cout << "error collision on " << psFen << std::endl;
+        exit(0);
+    }
+    myEvalHash[index] = egEvalEntry(key.getValue(), ef);
+}
+
+void Eval::initEG(){
+
+    // initiate table with zero entries
+    for (int i = 0; i < EG_HASH_SIZE; i++){
+        myEvalHash[i] = egEvalEntry();
+    }
+
+    // Add some generic draws
+    // 0. 2-man (KvsK) is draw
+    egHashAdd("k/K", &evaluateDraw);
+
+    // 3-man eval:
+    // King vs King + Bishop = insufficient material
+    egHashAdd("kb/K", &evaluateDraw);
+    egHashAdd("k/KB", &evaluateDraw);
+    // King vs King + Knight = insufficient material
+    egHashAdd("kn/K", &evaluateDraw);
+    egHashAdd("k/KN", &evaluateDraw);
+
+
+    // 4-man eval
+    // Obviously KN vs KB etc is draw also
+    egHashAdd("kn/KN", &evaluateDraw);
+    egHashAdd("kb/KB", &evaluateDraw);
+    egHashAdd("kn/KB", &evaluateDraw);
+    egHashAdd("kb/KN", &evaluateDraw);
+    // R vs R is also a draw
+    egHashAdd("kr/KR", &evaluateDraw);
+    // King vs King + two knights is a draw
+    egHashAdd("k/KNN", &evaluateDraw);
+    egHashAdd("knn/K", &evaluateDraw);
+    // Assume R vs B is always draw
+    egHashAdd("kb/KR", &evaluateDraw);
+    egHashAdd("kr/KB", &evaluateDraw);
+    // Same for R vs N
+    egHashAdd("kn/KR", &evaluateDraw);
+    egHashAdd("kr/KN", &evaluateDraw);
+
+    // 5-man eval
+    // lets say
+    // King, Rook, Bishop vs King and Rook
+    egHashAdd("krb/KR", &evaluateDraw);
+    egHashAdd("kr/KRB", &evaluateDraw);
+    // King, Rook, Knight vs King and Rook
+    egHashAdd("krn/KR", &evaluateDraw);
+    egHashAdd("kr/KRN", &evaluateDraw);
+}
---
+++ src/endgame.h
@@ -0,0 +1,33 @@
+#ifndef ENDGAME_H
+#define ENDGAME_H
+
+#include "defs.h"
+
+
+#define EG_HASH_SIZE        (1024)
+#define LARGEST_SP_EVAL     (5)
+
+typedef int (*egEvalFunction) (const Board &board, Color color);
+
+struct egEvalEntry{
+    U64            key;
+    egEvalFunction eFunction;
+
+    egEvalEntry(): key(0), eFunction(nullptr)  {};
+    egEvalEntry(U64 k, egEvalFunction ef): key(k), eFunction(ef) {};
+};
+
+// constant scores
+
+
+const int MIN_MATE_SCORE = 29000;
+const int MINIMAL_WON_SCORE      =  7500;
+const int EASY_WIN_SCORE         =  9500;
+const int CONFIDENT_WIN_SCORE    =  8500;
+
+const int DRAW_ZONE_ABS = 20;
+
+
+
+
+#endif
