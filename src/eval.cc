e7ca9d6de356f69290d92a509bbccf1cbe361982
justno4b <novosti-wxo@yandex.ru>
2022-10-06 00:34:53 +0300

    reduce

--- src/eval.cc
+++ src/eval.cc
@@ -957,7 +957,7 @@ inline int Eval::winnableEndgame(const Board & board, Color color, evalBits * eB
 
   int winnable = !pawnsBothFlanks * PAWNS_NOT_BOTH_PENALTY +
                   pawnEndgame * PAWN_ENDGAME_BONUS +
-                  !infiltration * (-42);
+                  !infiltration * (-21);
 
   s = gS(0, sign * std::max(winnable, -abs(eGpart)));
 
