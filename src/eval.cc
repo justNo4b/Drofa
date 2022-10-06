fa2a4a9fe21b44a1974d6325974e000f8ad53cb8
justno4b <novosti-wxo@yandex.ru>
2022-10-06 12:11:50 +0300

    up a bit

--- src/eval.cc
+++ src/eval.cc
@@ -952,7 +952,7 @@ inline int Eval::winnableEndgame(const Board & board, Color color, evalBits * eB
 
   int winnable = !pawnsBothFlanks * PAWNS_NOT_BOTH_PENALTY
                  + pawnEndgame * PAWN_ENDGAME_BONUS
-                 + 5 * outflanking;
+                 + 7 * outflanking;
 
   s = gS(0, sign * std::max(winnable, -abs(eGpart)));
 
