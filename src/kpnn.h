0ba3ee72de4e74a14e79884d3395893b82584456
justno4b <novosti-wxo@yandex.ru>
2023-01-11 23:29:47 +0300

    simplest net implementation

---
+++ src/kpnn.h
@@ -0,0 +1,22 @@
+#ifndef KPNN_H
+#define KPNN_H
+
+#include <math.h>
+
+
+// use net Pnet => 128x16x2 => opS /egS outputs
+#define N_INPUTS   (64 * 2)
+#define N_HIDDEN   (8)
+
+
+// net stuff
+double HIDDEN_WEIGHTS[N_INPUTS * N_HIDDEN] = {0};
+double OUTPUT_WEIGHTS1[N_HIDDEN] = {0};
+double OUTPUT_WEIGHTS2[N_HIDDEN] = {0};
+
+
+double sigmoid(double x){
+    return 1 / (1 + exp(-1 * x));
+}
+
+#endif
