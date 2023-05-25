27c302180c2d83020b48e35158041a0dcea875ca
justno4b <novosti-wxo@yandex.ru>
2023-05-25 12:45:19 +0300

    use isPmQuietCounter in lmp

--- src/search.cc
+++ src/search.cc
@@ -544,7 +544,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
       // 5. LATE MOVE PRUNING
       // If we made many quiet moves in the position already
       // we suppose other moves wont improve our situation
-      if ((qCount > _lmp_Array[depth][(improving || pvNode)]) && (moveHistory + cmHistory <= 0)) break;
+      if ((qCount > _lmp_Array[(depth - isPmQuietCounter)][(improving || pvNode)]) && (moveHistory + cmHistory <= 0)) break;
 
       // 6. SEE pruning of quiet moves
       // At shallow depth prune highlyish -negative SEE-moves
