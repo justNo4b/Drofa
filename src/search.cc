68378f7e6e03e7a16856c465caba3568f416b6da
justno4b <novosti-wxo@yandex.ru>
2023-06-09 00:42:08 +0300

    if bad move beaten beta, give it a bonus

--- src/search.cc
+++ src/search.cc
@@ -717,7 +717,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
         // Beta cutoff
         if (score >= beta) {
           // Add this move as a new killer move and update history if move is quiet
-          _updateBeta(isQuiet, move, board.getActivePlayer(), pMove, ply, (depth + 2 * (nodeEval < alpha)));
+          _updateBeta(isQuiet, move, board.getActivePlayer(), pMove, ply, (depth + (moveHistory + cmHistory <= 0) + 2 * (nodeEval < alpha)));
           // Award counter-move history additionally if we refuted special quite previous move
           if (isPmQuietCounter) _orderingInfo.incrementCounterHistory(board.getActivePlayer(), pMove, move.getPieceType(), move.getTo(), depth);
           // Add a new tt entry for this node
