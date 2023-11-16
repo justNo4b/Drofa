3e30860398305556c327fe67f64cf30c77ffa8ab
justno4b <novosti-wxo@yandex.ru>
2023-11-16 18:20:49 +0300

    try for cheap elo

--- src/search.cc
+++ src/search.cc
@@ -474,7 +474,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
   //
   // Drofa dont do this reduction after NullMove, because we already reduced a lot,
   // and reducing further may reduce quality of the NM_Search
-  if (depth >= 5 && !ttNode && pMove != 0 && !singSearch)
+  if (depth >= 5 && !ttNode && pMove != 0 && !singSearch && (cutNode || pvNode))
     depth--;
 
   // No pruning occured, generate moves and recurse
