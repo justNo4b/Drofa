8153dca4f511b8d6cb620fbe6ed75b5120ce2c46
justno4b <novosti-wxo@yandex.ru>
2023-11-23 14:52:37 +0300

    tweak

--- src/search.cc
+++ src/search.cc
@@ -474,7 +474,7 @@ int Search::_negaMax(const Board &board, pV *up_pV, int depth, int alpha, int be
   //
   // Drofa dont do this reduction after NullMove, because we already reduced a lot,
   // and reducing further may reduce quality of the NM_Search
-  if (depth >= 5 && !ttNode && pMove != 0 && !singSearch)
+  if (depth >= 5 && (!ttNode || ttMove == 0) && pMove != 0 && !singSearch)
     depth--;
 
   // No pruning occured, generate moves and recurse
